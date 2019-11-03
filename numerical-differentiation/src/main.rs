ce8615a706c7aedf7b438bd806acbbeb061c1546
Edward Steere <edward.steere@gmail.com>
2019-11-03 11:51:40 +0200

    Got numerical differentiation to compile
    It doesn't actually differentiate yet though.

--- numerical-differentiation/src/main.rs
+++ numerical-differentiation/src/main.rs
@@ -2,26 +2,27 @@ mod iterable;
 
 use iterable::iterable::*;
 
-fn next_approximation(n: f64, previous: f64) -> f64 {
-    (previous + n / previous) / 2.0
-}
-
-struct NumericalDifferentiation {
-    current: f64,
+struct NumericalDifferentiation<'a> {
+    h: f64,
     n: f64,
-    f: dyn Fn(f64) -> f64,
+    f: &'a dyn Fn(f64) -> f64,
 }
 
-impl Iterable<f64> for NumericalDifferentiation {
-    fn next(&self) -> &NumericalDifferentiation {
-        NewtonRhapsodyApproximation {
-            current: next_approximation(self.n, self.current),
+fn gradient<F>(n: f64, h: f64, f: F) -> f64 where F: Fn(f64) -> f64 {
+    (f(n + h) - f(n - h)) / (h * 2.0)
+}
+
+impl<'a> Iterable<f64> for NumericalDifferentiation<'a> {
+    fn next(&self) -> NumericalDifferentiation<'a> {
+        NumericalDifferentiation {
+            h: gradient(self.n, self.h, self.f),
             n: self.n,
+            f: self.f,
         }
     }
 
     fn get(&self) -> f64 {
-        self.current
+        self.h
     }
 
 }
@@ -42,19 +43,27 @@ fn relative(eps: f64, xs: &impl Iterable<f64>) -> impl Iterable<f64> {
     current
 }
 
+fn new_numerical_differentiation<'a>(n: f64, f: &'a dyn Fn(f64) -> f64) -> NumericalDifferentiation<'a> {
+    NumericalDifferentiation {
+        n: n,
+        h: 0.1,
+        f: f,
+    }
+}
+
 fn main() {
-    println!("sqrt 5: {:?}", naive_sqrts(5.0, 10));
+    let function = |x| 3.0 * (x * x) + 2.0 * x + 3.0;
     let after_a_couple =
-        drop(NewtonRhapsodyApproximation::new(5.0), 10).get();
+        drop(new_numerical_differentiation(5.0, &function), 10).get();
     println!("sqrt 5: {}", after_a_couple);
     let within_epsilon =
         within(
             0.00000000001,
-            &NewtonRhapsodyApproximation::new(5.0)).get();
+            &new_numerical_differentiation(5.0, &function)).get();
     println!("sqrt 5: {}", within_epsilon);
     let within_relative =
         relative(
             0.00000000001,
-            &NewtonRhapsodyApproximation::new(5.0)).get();
+            &new_numerical_differentiation(5.0, &function)).get();
     println!("sqrt 5: {}", within_relative);
 }
