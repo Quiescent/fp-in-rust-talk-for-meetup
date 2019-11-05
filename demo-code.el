a8a67e979d5ad5463c979d493987697cc79f1d77
Edward John Steere <edward.steere@gmail.com>
2019-11-05 08:13:23 +0200

    Fixup some of the plumbing in the presentation code

--- demo-code.el
+++ demo-code.el
@@ -5,31 +5,53 @@
 ;;; Code:
 
 (require 'demo-it)
+(require 'subr-x)
 
 (defun fir-load-line-in-main (start count)
   "Present a section of the main file.
 
 Show code from line START, ending COUNT lines after that."
   (progn
-    (demo-it-presentation-advance)
-    (demo-it-load-fancy-file "newton-rhapson-square-roots/src/main.rs" :line start (+ start count) :side)))
+    (demo-it-load-part-file "re-implement-std-lib/newton-rhapson-square-roots/src/main.rs"
+                            :line start (+ start count)
+                            :side
+                            1
+                            100)))
 
 (defun fir-load-next-approximation ()
   "Load the next approximation snippet of code."
+  (demo-it-presentation-advance)
   (fir-load-line-in-main 5 3))
 
 (defun fir-load-series-of-approximations ()
   "Load the series of approximations snippet of code."
+  (demo-it-presentation-advance)
   (fir-load-line-in-main 9 8))
+
+(defun fir-cleanup ()
+  "Cleanup all temp buffers used in presentation."
+  (thread-last (buffer-list)
+    (cl-remove-if-not (lambda (buffer) (string-match "main\\.rs"
+                                                     (buffer-name buffer))))
+    (mapc #'kill-buffer)))
+
+(defun fir-close-code-window ()
+  "Close the code window."
+  (progn
+    (other-window 1)
+    (delete-other-windows)
+    (fir-cleanup)))
 
 (defun fir-setup-presentation ()
   "Entry point for the code presentation."
   (interactive)
   (progn
+    (fir-cleanup)
     (demo-it-create :single-window
                     (demo-it-title-screen "code-title.org")
                     (demo-it-presentation "code-presentation.org")
                     fir-load-next-approximation
+                    fir-close-code-window
                     fir-load-series-of-approximations)
     (demo-it-start)))
 
